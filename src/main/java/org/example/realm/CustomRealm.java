5e98d36705ff733bc3284c78d1070ff35f6304e3
Chauncey <1723446@qq.com>
2021-01-05 15:10:18 +0800

    feat[realm]: 自定义实现realm

---
+++ src/main/java/org/example/realm/CustomRealm.java
@@ -0,0 +1,36 @@
+package org.example.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * 自定义realm, 将认证、授权数据来源转换为数据库
+ */
+public class CustomRealm extends AuthorizingRealm {
+    /**
+     * 授权
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    /**
+     * 认证
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        // 从token中获取用户名
+        String principal = (String) authenticationToken.getPrincipal();
+        // 根据用户名查询数据库
+        if ("Chauncey".equals(principal)) {
+            return new SimpleAuthenticationInfo(principal, "123456", this.getName());
+        }
+        return null;
+    }
+}
