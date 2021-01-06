c18db1085764af2d2e34fa48aff8cbe54d996815
Chauncey <1723446@qq.com>
2021-01-06 10:32:48 +0800

    feat[realm]: 提取密码为单独变量

--- src/main/java/org/example/realm/CustomMD5Realm.java
+++ src/main/java/org/example/realm/CustomMD5Realm.java
@@ -28,11 +28,13 @@ public class CustomMD5Realm extends AuthorizingRealm {
         // 从token中获取用户名
         String principal = (String) authenticationToken.getPrincipal();
         if ("Chauncey".equals(principal)) {
-            return new SimpleAuthenticationInfo(
-                    principal,
-                    "e10adc3949ba59abbe56e057f20f883e",
-                    this.getName()
-            );
+            // MD5
+            String password = "e10adc3949ba59abbe56e057f20f883e";
+            // MD5 + Salt
+            // String passwordWithSalt = "5f9dcb70720b6f58a2b219015fad5c30";
+            // MD5 + Salt + Hash
+            // String passwordWithSaltAndHash = "14b9f6ecccb925f15cf92c44ff9326ce";
+            return new SimpleAuthenticationInfo(principal, password, this.getName());
         }
         return null;
     }
