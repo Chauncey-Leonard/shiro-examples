8705713718d085f0a58a8146d3fa34c610f4a0ca
Chauncey <1723446@qq.com>
2021-02-19 14:15:09 +0800

    feat: 创建自定义realm

--- springboot-shiro/src/main/java/com/example/config/shiro/CustomRealm.java
+++ springboot-shiro/src/main/java/com/example/config/shiro/CustomRealm.java
@@ -1,5 +1,6 @@
 package com.example.config.shiro;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -10,12 +11,15 @@ import org.apache.shiro.subject.PrincipalCollection;
 /**
  * @author Chauncey
  */
+@Slf4j
 public class CustomRealm extends AuthorizingRealm {
+
     /**
      * 授权
      */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        log.info("********************** 授权执行中 ***************************");
         return null;
     }
 
@@ -24,6 +28,7 @@ public class CustomRealm extends AuthorizingRealm {
      */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        log.info("********************** 认证执行中 ***************************");
         return null;
     }
 }
